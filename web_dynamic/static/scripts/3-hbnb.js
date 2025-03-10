aa8a0ac89f60f8b9f5e2e3eb89f0ff7303c37783
joana.casallas <joacasallas@gmail.com>
2025-03-10 10:32:26 -0500

    fix fetch places_search

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
 /* global $ */
 $(document).ready(function () {
+  listAmenitiesChecked();
   checkStatus();
   places();
+});
+
+function listAmenitiesChecked() {
   const listAmenitiesChecked = [];
   $('.amenities input[type="checkbox"]').change(function () {
     if ($(this).is(":checked")) {
@@ -15,7 +19,7 @@ $(document).ready(function () {
     }
     $(".amenities h4").text(listAmenitiesChecked.join(", "));
   });
-});
+}
 
 function checkStatus() {
   $.get("http://127.0.0.1:5001/api/v1/status")
@@ -38,38 +42,40 @@ function checkStatus() {
 }
 
 function places() {
-  $.post({
+  $.ajax({
+    type: "post",
     url: "http://127.0.0.1:5001/api/v1/places_search/",
     contentType: "application/json",
     data: JSON.stringify({}),
-  })
-    .done(function (places) {
-      places.forEach((place) => {`
-          <article>
+    success: function (places) {
+      for (const place of places) {
+        const placeHtml = `
+        <article>
           <div class="title_box">
-            <h2>${place.name}</h2>
-            <div class="price_by_night">$${place.price_by_night}</div>
+            <h2>${ place.name }</h2>
+            <div class="price_by_night">$${ place.price_by_night }</div>
           </div>
           <div class="information">
             <div class="max_guest">
-              ${place.max_guest} Guest${place.max_guest !== 1 ? "s" : ""}
+              ${ place.max_guest } Guest${place.max_guest !== 1 ? "s": ""}
             </div>
             <div class="number_rooms">
-              ${place.number_rooms} Bedroom${place.number_rooms !== 1 ? "s" : ""}
+              ${ place.number_rooms } Bedroom${place.number_rooms !== 1 ? "s":""}
             </div>
             <div class="number_bathrooms">
-              ${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? "s" : ""}
+              ${ place.number_bathrooms } Bathroom${place.number_bathrooms !== 1 ? "s": ""}
             </div>
           </div>
           <div class="user">
-            <b>Owner:</b> ${place.user?.first_name || "Unknown"} ${place.user?.last_name || "Unknown"}
+            <b>Owner:</b> ${ place.user?.first_name ?? "undefined" } ${ place.user?.last_name ?? "undefned" }
           </div>
-          <div class="description">${place.description}</div>
-        </article>`;
-      });
-    })
-    .fail(function (xhr, status, error) {
-      console.error("Cannot connect: ", status, error);
-      console.error("Response Text: ", xhr.responseText);
-    });
+          <div class="description">${ place.description}</div>
+        </article>`
+        $('.places').append(placeHtml);
+      }
+    },
+    error: function (xhr, status, error) {
+      console.error("connection error: ", status, error);
+    },
+  });
 }
