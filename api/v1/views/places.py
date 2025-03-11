ef3900b131cb19d2c0fb668d52698eb4cc4f9cbc
joana.casallas <joacasallas@gmail.com>
2025-03-11 10:40:19 -0500

    6. States and Cities

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -179,6 +179,9 @@ def places_search():
     for p in list_places:
         d = p.to_dict()
         d.pop('amenities', None)
+        user = storage.get(User, p.user_id)
+        d["user_first_name"] = user.first_name
+        d["user_last_name"] = user.last_name
         places.append(d)
 
     return jsonify(places)
